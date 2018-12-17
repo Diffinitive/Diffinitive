c03d2c569f08f30e9583c00ffadf01cf3c7a910b
Jonatan Werpers <jonatan@werpers.com>
2018-12-17 12:53:00 +0100

    Add 1d stencil type

--- sbpD2.jl
+++ sbpD2.jl
@@ -10,3 +10,32 @@ end
 function closureSize(D::D2)::Int
     return length(quadratureClosure)
 end
+
+struct Stencil
+    range::NTuple{2,Int}
+    weights::Vector{Float64}
+    function Stencil(range, weights)
+        width = range[2]-range[1]+1
+        if width != length(weights)
+            error("The width and the number of weights must be the same")
+        end
+        new(range, weights)
+    end
+end
+
+# Provides index into the Stencil based on offset for the root element
+function Base.getindex(s::Stencil, i::Int)
+    if s.range[1] <= i <= s.range[2]
+        return s.weights[1 + i - s.range[1]]
+    else
+        return 0
+    end
+end
+
+function apply(s::Stencil, v::AbstractVector, i::Int)
+    w = zero(v[0])
+    for j ∈ i+(s.range[1]:s.range[2])
+        w += v[j]
+    end
+    return w
+end
