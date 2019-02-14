dae3c3e2cc0c9ba82c6d92a082a473a088bc595a
Jonatan Werpers <jonatan@werpers.com>
2019-02-14 12:46:58 +0100

    Improve stencil application code to make it more friendly to compiler optimizations
    --HG--
    branch : cell_based_test

--- stencil.jl
+++ stencil.jl
@@ -9,24 +9,22 @@ function flip(s::Stencil)
 end
 
 # Provides index into the Stencil based on offset for the root element
-function Base.getindex(s::Stencil, i::Int)
+@inline function Base.getindex(s::Stencil, i::Int)
     @boundscheck if i < s.range[1] || s.range[2] < i
         return eltype(s.weights)(0)
     end
-
     return s.weights[1 + i - s.range[1]]
 end
 
-Base.@propagate_inbounds function apply(s::Stencil, v::AbstractVector, i::Int)
+Base.@propagate_inbounds @inline function apply(s::Stencil{T,N}, v::AbstractVector, i::Int) where {T,N}
     w = zero(eltype(v))
-    for j ∈ s.range[1]:s.range[2]
-        @inbounds weight = s[j]
-        w += weight*v[i+j]
+    @simd for k ∈ 1:N
+        w += s.weights[k]*v[i+ s.range[1] + k-1]
     end
     return w
 end
 
-Base.@propagate_inbounds function apply_backwards(s::Stencil, v::AbstractVector, i::Int)
+Base.@propagate_inbounds @inline function apply_backwards(s::Stencil, v::AbstractVector, i::Int)
     w = zero(eltype(v))
     for j ∈ s.range[2]:-1:s.range[1]
         @inbounds weight = s[j]
