3152fe3a4f067c1332f9057a52e710884b5f17dd
Jonatan Werpers <jonatan@werpers.com>
2019-05-21 13:42:51 +0200

    Remove grid module
    --HG--
    branch : boundary_conditions

--- grid.jl
+++ grid.jl
@@ -1,5 +1,3 @@
-module Grid
-
 abstract type BoundaryIdentifier end
 struct CartesianBoundary{Dim, R<:Region} <: BoundaryIdentifier end
 dim(::CartesianBoundary{Dim, R}) where {Dim, R} = Dim
@@ -7,5 +5,3 @@ region(::CartesianBoundary{Dim, R}) where {Dim, R} = R
 
 include("AbstractGrid.jl")
 include("EquidistantGrid.jl")
-
-end
