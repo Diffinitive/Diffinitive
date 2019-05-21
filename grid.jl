cb8c4754fe753e16a165b421a2d13c15c484ef6d
Jonatan Werpers <jonatan@werpers.com>
2019-05-21 10:46:39 +0200

    Add getter functions for dim and region on a cartesian boundary identifier
    --HG--
    branch : boundary_conditions

--- grid.jl
+++ grid.jl
@@ -2,6 +2,8 @@ module Grid
 
 abstract type BoundaryIdentifier end
 struct CartesianBoundary{Dim, R<:Region} <: BoundaryIdentifier end
+dim(::CartesianBoundary{Dim, R}) where {Dim, R} = Dim
+region(::CartesianBoundary{Dim, R}) where {Dim, R} = R
 
 include("AbstractGrid.jl")
 include("EquidistantGrid.jl")
