11ab00daca42c79f26b11a8032fcf9e82e6c39bf
Jonatan Werpers <jonatan@werpers.com>
2019-06-26 13:13:56 +0200

    Delete grid.jl that should've been deleted before
    --HG--
    branch : package_refactor

--- grid.jl
+++
@@ -1,7 +0,0 @@
-abstract type BoundaryIdentifier end
-struct CartesianBoundary{Dim, R<:Region} <: BoundaryIdentifier end
-dim(::CartesianBoundary{Dim, R}) where {Dim, R} = Dim
-region(::CartesianBoundary{Dim, R}) where {Dim, R} = R
-
-include("AbstractGrid.jl")
-include("EquidistantGrid.jl")
