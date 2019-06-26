da1b83087447e09941d4ba4914b466cc7765766a
Jonatan Werpers <jonatan@werpers.com>
2019-06-26 10:44:20 +0200

    Move DiffOps and Grids into packages
    --HG--
    branch : package_refactor

---
+++ Grids/src/Grids.jl
@@ -0,0 +1,11 @@
+module Grids
+
+abstract type BoundaryIdentifier end
+struct CartesianBoundary{Dim, R<:Region} <: BoundaryIdentifier end
+dim(::CartesianBoundary{Dim, R}) where {Dim, R} = Dim
+region(::CartesianBoundary{Dim, R}) where {Dim, R} = R
+
+include("AbstractGrid.jl")
+include("EquidistantGrid.jl")
+
+end # module
--- sbp.jl
+++ sbp.jl
@@ -1,8 +1,6 @@
 module sbp
 include("index.jl")
-include("grid.jl")
 include("stencil.jl")
 include("sbpD2.jl")
-include("diffOp.jl")
 include("TimeStepper.jl")
 end  # module
