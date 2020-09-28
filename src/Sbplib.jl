08a6579c681502eaf3010cc419df4968167c6db4
Jonatan Werpers <jonatan@werpers.com>
2020-09-28 22:46:37 +0200

    Reinclude DiffOps.jl
    --HG--
    branch : refactor/remove_dynamic_size_tensormapping

--- src/Sbplib.jl
+++ src/Sbplib.jl
@@ -4,7 +4,7 @@ include("RegionIndices/RegionIndices.jl")
 include("LazyTensors/LazyTensors.jl")
 include("Grids/Grids.jl")
 include("SbpOperators/SbpOperators.jl")
-# include("DiffOps/DiffOps.jl")
+include("DiffOps/DiffOps.jl")
 
 export RegionIndices
 export LazyTensors
