4ee52786dc308a747d73d0d7f367ae68f9e9f65b
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2021-01-04 16:59:11 +0100

    remove extra space
    --HG--
    branch : feature/volume_and_boundary_operators

--- src/SbpOperators/volumeops/quadratures/quadrature.jl
+++ src/SbpOperators/volumeops/quadratures/quadrature.jl
@@ -9,7 +9,7 @@ for the points in the closure regions.
 
 On a one-dimensional `grid`, `H` is a `VolumeOperator`. On a multi-dimensional
 `grid`, `H` is the outer product of the 1-dimensional quadrature operators in
-each  coordinate direction. Also see the documentation of
+each coordinate direction. Also see the documentation of
 `SbpOperators.volume_operator(...)` for more details.
 """
 function Quadrature(grid::EquidistantGrid{Dim}, inner_stencil, closure_stencils) where Dim
