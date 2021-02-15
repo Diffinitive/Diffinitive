c76cee5bf67f8d09b1ce2bbf7f99f0fac4a3b346
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2021-02-15 17:59:51 +0100

    Remove obsolete opset from tests
    --HG--
    branch : feature/laplace_opset

--- test/testSbpOperators.jl
+++ test/testSbpOperators.jl
@@ -336,28 +336,27 @@ end
 @testset "Laplace" begin
     g_1D = EquidistantGrid(101, 0.0, 1.)
     g_3D = EquidistantGrid((51,101,52), (0.0, -1.0, 0.0), (1., 1., 1.))
-    op2 = read_D2_operator(sbp_operators_path()*"standard_diagonal.toml"; order=4)
-    op4 = read_D2_operator(sbp_operators_path()*"standard_diagonal.toml"; order=4)
+    op = read_D2_operator(sbp_operators_path()*"standard_diagonal.toml"; order=4)
     @testset "Constructors" begin
 
         @testset "1D" begin
             # Create all tensor mappings included in Laplace
-            Δ = laplace(g_1D, op4.innerStencil, op4.closureStencils)
-            H = inner_product(g_1D, op4.quadratureClosure)
-            Hi = inverse_inner_product(g_1D, op4.quadratureClosure)
+            Δ = laplace(g_1D, op.innerStencil, op.closureStencils)
+            H = inner_product(g_1D, op.quadratureClosure)
+            Hi = inverse_inner_product(g_1D, op.quadratureClosure)
 
             (id_l, id_r) = boundary_identifiers(g_1D)
 
-            e_l = boundary_restriction(g_1D,op4.eClosure,id_l)
-            e_r = boundary_restriction(g_1D,op4.eClosure,id_r)
+            e_l = boundary_restriction(g_1D,op.eClosure,id_l)
+            e_r = boundary_restriction(g_1D,op.eClosure,id_r)
             e_dict = Dict(Pair(id_l,e_l),Pair(id_r,e_r))
 
-            d_l = normal_derivative(g_1D,op4.dClosure,id_l)
-            d_r = normal_derivative(g_1D,op4.dClosure,id_r)
+            d_l = normal_derivative(g_1D,op.dClosure,id_l)
+            d_r = normal_derivative(g_1D,op.dClosure,id_r)
             d_dict = Dict(Pair(id_l,d_l),Pair(id_r,d_r))
 
-            H_l = inner_product(boundary_grid(g_1D,id_l),op4.quadratureClosure)
-            H_r = inner_product(boundary_grid(g_1D,id_r),op4.quadratureClosure)
+            H_l = inner_product(boundary_grid(g_1D,id_l),op.quadratureClosure)
+            H_r = inner_product(boundary_grid(g_1D,id_r),op.quadratureClosure)
             Hb_dict = Dict(Pair(id_l,H_l),Pair(id_r,H_r))
 
             L = Laplace(g_1D, sbp_operators_path()*"standard_diagonal.toml"; order=4)
@@ -367,38 +366,38 @@ end
         end
         @testset "3D" begin
             # Create all tensor mappings included in Laplace
-            Δ = laplace(g_3D, op4.innerStencil, op4.closureStencils)
-            H = inner_product(g_3D, op4.quadratureClosure)
-            Hi = inverse_inner_product(g_3D, op4.quadratureClosure)
+            Δ = laplace(g_3D, op.innerStencil, op.closureStencils)
+            H = inner_product(g_3D, op.quadratureClosure)
+            Hi = inverse_inner_product(g_3D, op.quadratureClosure)
 
             (id_l, id_r, id_s, id_n, id_b, id_t) = boundary_identifiers(g_3D)
 
-            e_l = boundary_restriction(g_3D,op4.eClosure,id_l)
-            e_r = boundary_restriction(g_3D,op4.eClosure,id_r)
-            e_s = boundary_restriction(g_3D,op4.eClosure,id_s)
-            e_n = boundary_restriction(g_3D,op4.eClosure,id_n)
-            e_b = boundary_restriction(g_3D,op4.eClosure,id_b)
-            e_t = boundary_restriction(g_3D,op4.eClosure,id_t)
+            e_l = boundary_restriction(g_3D,op.eClosure,id_l)
+            e_r = boundary_restriction(g_3D,op.eClosure,id_r)
+            e_s = boundary_restriction(g_3D,op.eClosure,id_s)
+            e_n = boundary_restriction(g_3D,op.eClosure,id_n)
+            e_b = boundary_restriction(g_3D,op.eClosure,id_b)
+            e_t = boundary_restriction(g_3D,op.eClosure,id_t)
             e_dict = Dict(Pair(id_l,e_l),Pair(id_r,e_r),
                           Pair(id_s,e_s),Pair(id_n,e_n),
                           Pair(id_b,e_b),Pair(id_t,e_t))
 
-            d_l = normal_derivative(g_3D,op4.dClosure,id_l)
-            d_r = normal_derivative(g_3D,op4.dClosure,id_r)
-            d_s = normal_derivative(g_3D,op4.dClosure,id_s)
-            d_n = normal_derivative(g_3D,op4.dClosure,id_n)
-            d_b = normal_derivative(g_3D,op4.dClosure,id_b)
-            d_t = normal_derivative(g_3D,op4.dClosure,id_t)
+            d_l = normal_derivative(g_3D,op.dClosure,id_l)
+            d_r = normal_derivative(g_3D,op.dClosure,id_r)
+            d_s = normal_derivative(g_3D,op.dClosure,id_s)
+            d_n = normal_derivative(g_3D,op.dClosure,id_n)
+            d_b = normal_derivative(g_3D,op.dClosure,id_b)
+            d_t = normal_derivative(g_3D,op.dClosure,id_t)
             d_dict = Dict(Pair(id_l,d_l),Pair(id_r,d_r),
                           Pair(id_s,d_s),Pair(id_n,d_n),
                           Pair(id_b,d_b),Pair(id_t,d_t))
 
-            H_l = inner_product(boundary_grid(g_3D,id_l),op4.quadratureClosure)
-            H_r = inner_product(boundary_grid(g_3D,id_r),op4.quadratureClosure)
-            H_s = inner_product(boundary_grid(g_3D,id_s),op4.quadratureClosure)
-            H_n = inner_product(boundary_grid(g_3D,id_n),op4.quadratureClosure)
-            H_b = inner_product(boundary_grid(g_3D,id_b),op4.quadratureClosure)
-            H_t = inner_product(boundary_grid(g_3D,id_t),op4.quadratureClosure)
+            H_l = inner_product(boundary_grid(g_3D,id_l),op.quadratureClosure)
+            H_r = inner_product(boundary_grid(g_3D,id_r),op.quadratureClosure)
+            H_s = inner_product(boundary_grid(g_3D,id_s),op.quadratureClosure)
+            H_n = inner_product(boundary_grid(g_3D,id_n),op.quadratureClosure)
+            H_b = inner_product(boundary_grid(g_3D,id_b),op.quadratureClosure)
+            H_t = inner_product(boundary_grid(g_3D,id_t),op.quadratureClosure)
             Hb_dict = Dict(Pair(id_l,H_l),Pair(id_r,H_r),
                           Pair(id_s,H_s),Pair(id_n,H_n),
                           Pair(id_b,H_b),Pair(id_t,H_t))
@@ -412,16 +411,16 @@ end
 
     @testset "laplace" begin
         @testset "1D" begin
-            L = laplace(g_1D, op4.innerStencil, op4.closureStencils)
-            @test L == second_derivative(g_1D, op4.innerStencil, op4.closureStencils)
+            L = laplace(g_1D, op.innerStencil, op.closureStencils)
+            @test L == second_derivative(g_1D, op.innerStencil, op.closureStencils)
             @test L isa TensorMapping{T,1,1}  where T
         end
         @testset "3D" begin
-            L = laplace(g_3D, op4.innerStencil, op4.closureStencils)
+            L = laplace(g_3D, op.innerStencil, op.closureStencils)
             @test L isa TensorMapping{T,3,3} where T
-            Dxx = second_derivative(g_3D, op4.innerStencil, op4.closureStencils,1)
-            Dyy = second_derivative(g_3D, op4.innerStencil, op4.closureStencils,2)
-            Dzz = second_derivative(g_3D, op4.innerStencil, op4.closureStencils,3)
+            Dxx = second_derivative(g_3D, op.innerStencil, op.closureStencils,1)
+            Dyy = second_derivative(g_3D, op.innerStencil, op.closureStencils,2)
+            Dzz = second_derivative(g_3D, op.innerStencil, op.closureStencils,3)
             @test L == Dxx + Dyy + Dzz
             @test L isa TensorMapping{T,3,3} where T
         end
@@ -429,12 +428,12 @@ end
 
     @testset "inner_product" begin
         L = Laplace(g_3D, sbp_operators_path()*"standard_diagonal.toml"; order=4)
-        @test inner_product(L) == inner_product(g_3D,op4.quadratureClosure)
+        @test inner_product(L) == inner_product(g_3D,op.quadratureClosure)
     end
 
     @testset "inverse_inner_product" begin
         L = Laplace(g_3D, sbp_operators_path()*"standard_diagonal.toml"; order=4)
-        @test inverse_inner_product(L) == inverse_inner_product(g_3D,op4.quadratureClosure)
+        @test inverse_inner_product(L) == inverse_inner_product(g_3D,op.quadratureClosure)
     end
 
     @testset "boundary_restriction" begin
@@ -445,12 +444,12 @@ end
         id_n = CartesianBoundary{2,Upper}()
         id_b = CartesianBoundary{3,Lower}()
         id_t = CartesianBoundary{3,Upper}()
-        @test boundary_restriction(L,id_l) == boundary_restriction(g_3D,op4.eClosure,id_l)
-        @test boundary_restriction(L,id_r) == boundary_restriction(g_3D,op4.eClosure,id_r)
-        @test boundary_restriction(L,id_s) == boundary_restriction(g_3D,op4.eClosure,id_s)
-        @test boundary_restriction(L,id_n) == boundary_restriction(g_3D,op4.eClosure,id_n)
-        @test boundary_restriction(L,id_b) == boundary_restriction(g_3D,op4.eClosure,id_b)
-        @test boundary_restriction(L,id_t) == boundary_restriction(g_3D,op4.eClosure,id_t)
+        @test boundary_restriction(L,id_l) == boundary_restriction(g_3D,op.eClosure,id_l)
+        @test boundary_restriction(L,id_r) == boundary_restriction(g_3D,op.eClosure,id_r)
+        @test boundary_restriction(L,id_s) == boundary_restriction(g_3D,op.eClosure,id_s)
+        @test boundary_restriction(L,id_n) == boundary_restriction(g_3D,op.eClosure,id_n)
+        @test boundary_restriction(L,id_b) == boundary_restriction(g_3D,op.eClosure,id_b)
+        @test boundary_restriction(L,id_t) == boundary_restriction(g_3D,op.eClosure,id_t)
     end
 
     @testset "normal_derivative" begin
@@ -461,12 +460,12 @@ end
         id_n = CartesianBoundary{2,Upper}()
         id_b = CartesianBoundary{3,Lower}()
         id_t = CartesianBoundary{3,Upper}()
-        @test normal_derivative(L,id_l) == normal_derivative(g_3D,op4.dClosure,id_l)
-        @test normal_derivative(L,id_r) == normal_derivative(g_3D,op4.dClosure,id_r)
-        @test normal_derivative(L,id_s) == normal_derivative(g_3D,op4.dClosure,id_s)
-        @test normal_derivative(L,id_n) == normal_derivative(g_3D,op4.dClosure,id_n)
-        @test normal_derivative(L,id_b) == normal_derivative(g_3D,op4.dClosure,id_b)
-        @test normal_derivative(L,id_t) == normal_derivative(g_3D,op4.dClosure,id_t)
+        @test normal_derivative(L,id_l) == normal_derivative(g_3D,op.dClosure,id_l)
+        @test normal_derivative(L,id_r) == normal_derivative(g_3D,op.dClosure,id_r)
+        @test normal_derivative(L,id_s) == normal_derivative(g_3D,op.dClosure,id_s)
+        @test normal_derivative(L,id_n) == normal_derivative(g_3D,op.dClosure,id_n)
+        @test normal_derivative(L,id_b) == normal_derivative(g_3D,op.dClosure,id_b)
+        @test normal_derivative(L,id_t) == normal_derivative(g_3D,op.dClosure,id_t)
     end
 
     @testset "boundary_quadrature" begin
@@ -477,12 +476,12 @@ end
         id_n = CartesianBoundary{2,Upper}()
         id_b = CartesianBoundary{3,Lower}()
         id_t = CartesianBoundary{3,Upper}()
-        @test boundary_quadrature(L,id_l) == inner_product(boundary_grid(g_3D,id_l),op4.quadratureClosure)
-        @test boundary_quadrature(L,id_r) == inner_product(boundary_grid(g_3D,id_r),op4.quadratureClosure)
-        @test boundary_quadrature(L,id_s) == inner_product(boundary_grid(g_3D,id_s),op4.quadratureClosure)
-        @test boundary_quadrature(L,id_n) == inner_product(boundary_grid(g_3D,id_n),op4.quadratureClosure)
-        @test boundary_quadrature(L,id_b) == inner_product(boundary_grid(g_3D,id_b),op4.quadratureClosure)
-        @test boundary_quadrature(L,id_t) == inner_product(boundary_grid(g_3D,id_t),op4.quadratureClosure)
+        @test boundary_quadrature(L,id_l) == inner_product(boundary_grid(g_3D,id_l),op.quadratureClosure)
+        @test boundary_quadrature(L,id_r) == inner_product(boundary_grid(g_3D,id_r),op.quadratureClosure)
+        @test boundary_quadrature(L,id_s) == inner_product(boundary_grid(g_3D,id_s),op.quadratureClosure)
+        @test boundary_quadrature(L,id_n) == inner_product(boundary_grid(g_3D,id_n),op.quadratureClosure)
+        @test boundary_quadrature(L,id_b) == inner_product(boundary_grid(g_3D,id_b),op.quadratureClosure)
+        @test boundary_quadrature(L,id_t) == inner_product(boundary_grid(g_3D,id_t),op.quadratureClosure)
     end
 
     # Exact differentiation is measured point-wise. In other cases
