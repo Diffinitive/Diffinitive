ec36d820428f14dac8dd72f3d243fd75b9383633
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2022-01-18 22:09:55 +0100

    Fix typos in doc.
    --HG--
    branch : operator_storage_array_of_table

--- src/StaticDicts/StaticDicts.jl
+++ src/StaticDicts/StaticDicts.jl
@@ -10,9 +10,9 @@ A static dictionary implementing the interface for an `AbstractDict`. A
 
 The immutable nature means that `StaticDict` can be compared with `===`, in
 constrast to regular `Dict` or `ImmutableDict` which can not. (See
-<https://github.com/JuliaLang/julia/issues/4648> for details) One important
+<https://github.com/JuliaLang/julia/issues/4648> for details.) One important
 aspect of this is that `StaticDict` can be used in a struct while still
-allowing the struct to be comared using the default implementation of `==` for
+allowing the struct to be compared using the default implementation of `==` for
 structs.
 
 Lookups are done by linear search.
