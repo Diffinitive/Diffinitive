0c662d70cb35297e01c85ea384759ce28f3e8946
Jonatan Werpers <jonatan@werpers.com>
2022-01-28 10:38:58 +0100

    Wrap 1D tests in a testset
    --HG--
    branch : feature/variable_derivatives

--- test/SbpOperators/volumeops/derivatives/second_derivative_variable_test.jl
+++ test/SbpOperators/volumeops/derivatives/second_derivative_variable_test.jl
@@ -16,33 +16,35 @@ using Sbplib.SbpOperators: NestedStencil, CenteredNestedStencil
         NestedStencil(( 2.,  -1., 0.),(-3., 1.,  0.), (1., 0., 0.), center = 1),
     ]
 
-    @testset "Constructors" begin
-        @test SecondDerivativeVariable(interior_stencil,Tuple(closure_stencils), (4,), c) isa TensorMapping
-        @test SecondDerivativeVariable(g, c, interior_stencil, closure_stencils) isa TensorMapping
-    end
-
-    @testset "sizes" begin
-        D₂ᶜ = SecondDerivativeVariable(g, c, interior_stencil, closure_stencils)
-        @test closure_size(D₂ᶜ) == 1
-        @test range_size(D₂ᶜ) == (11,)
-        @test domain_size(D₂ᶜ) == (11,)
-    end
-
-    @testset "application" begin
-
-        function apply_to_functions(;v,c)
-            g = EquidistantGrid(11, 0., 10.) # h = 1
-            c̄ = evalOn(g,c)
-            v̄ = evalOn(g,v)
-
-            D₂ᶜ = SecondDerivativeVariable(g, c̄, interior_stencil, closure_stencils)
-            return D₂ᶜ*v̄
+    @testset "1D" begin
+        @testset "Constructors" begin
+            @test SecondDerivativeVariable(interior_stencil,Tuple(closure_stencils), (4,), c) isa TensorMapping
+            @test SecondDerivativeVariable(g, c, interior_stencil, closure_stencils) isa TensorMapping
         end
 
-        @test apply_to_functions(v=x->1., c=x->-1.) == zeros(11)
-        @test apply_to_functions(v=x->1., c=x->-x) == zeros(11)
-        @test apply_to_functions(v=x->x, c=x-> 1.) == zeros(11)
-        @test apply_to_functions(v=x->x, c=x->-x) == -ones(11)
+        @testset "sizes" begin
+            D₂ᶜ = SecondDerivativeVariable(g, c, interior_stencil, closure_stencils)
+            @test closure_size(D₂ᶜ) == 1
+            @test range_size(D₂ᶜ) == (11,)
+            @test domain_size(D₂ᶜ) == (11,)
+        end
+
+        @testset "application" begin
+
+            function apply_to_functions(;v,c)
+                g = EquidistantGrid(11, 0., 10.) # h = 1
+                c̄ = evalOn(g,c)
+                v̄ = evalOn(g,v)
+
+                D₂ᶜ = SecondDerivativeVariable(g, c̄, interior_stencil, closure_stencils)
+                return D₂ᶜ*v̄
+            end
+
+            @test apply_to_functions(v=x->1., c=x->-1.) == zeros(11)
+            @test apply_to_functions(v=x->1., c=x->-x) == zeros(11)
+            @test apply_to_functions(v=x->x, c=x-> 1.) == zeros(11)
+            @test apply_to_functions(v=x->x, c=x->-x) == -ones(11)
+        end
     end
 end
 
