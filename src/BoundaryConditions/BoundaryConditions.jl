f15af1f73e67679c20bf95406fbeb208c4089e3d
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2022-10-11 18:12:40 +0200

    Add type for different kind of boundary data and refactor the BoundaryConditions such that they are general w.r.t the boundary data type
    --HG--
    branch : feature/boundary_conditions

--- src/BoundaryConditions/BoundaryConditions.jl
+++ src/BoundaryConditions/BoundaryConditions.jl
@@ -1,12 +1,24 @@
 module BoundaryConditions
 
+export BoundaryDataType
+export ConstantBoundaryData
+export SpaceDependentBoundaryData
+export TimeDependentBoundaryData
+export SpaceDependentBoundaryData
+
 export BoundaryCondition
-export BoundaryConditionType
-export Neumann
-export Dirichlet
+export NeumannCondition
+export DirichletCondition
+export RobinCondition
+
+export data
+export sat
+export sat_tensors
 
 using Sbplib.Grids
+using Sbplib.LazyTensors
 
 include("boundary_condition.jl")
+include("sat.jl")
 
 end # module
--- src/BoundaryConditions/boundary_condition.jl
+++ src/BoundaryConditions/boundary_condition.jl
@@ -1,11 +1,52 @@
-abstract type BoundaryConditionType end
-struct Dirichlet <: BoundaryConditionType end
-struct Neumann <: BoundaryConditionType end
+"""
+    BoundaryDataType
 
-struct BoundaryCondition{BCType <: BoundaryConditionType, ID <: BoundaryIdentifier, DType}
-    id::ID
-    data::DType
-    BoundaryCondition{BCType}(id::ID, data::DType) where {BCType <: BoundaryConditionType,
-                                                          ID <: BoundaryIdentifier,
-                                                          DType} = new{BCType,ID,DType}(id, data)
+A type for storing boundary data, e.g. constant, space-dependent, time-dependent etc.
+Subtypes of `BoundaryDataType` should store the boundary data in a field `val`, i.e.
+`struct MyBoundaryDataType{T} <: BoundaryDataType val::T end`.
+"""
+abstract type BoundaryDataType end
+
+struct ConstantBoundaryData{T} <: BoundaryDataType
+    val::T
+end
+
+struct SpaceDependentBoundaryData{T} <: BoundaryDataType
+    val::T
+end
+
+struct TimeDependentBoundaryData{T} <: BoundaryDataType
+    val::T
+end
+
+struct SpaceTimeDependentBoundaryData{T} <: BoundaryDataType
+    val::T
+end
+
+"""
+    BoundaryCondition
+
+A type for implementing data needed in order to impose a boundary condition.
+Subtypes refer to perticular types of boundary conditions, e.g. Neumann conditions.
+"""
+# TODO: Parametrize the boundary id as well?
+abstract type BoundaryCondition{T<:BoundaryDataType} end
+
+data(bc::BoundaryCondition) = bc.data.val
+
+struct NeumannCondition{BDT<:BoundaryDataType} <: BoundaryCondition{BDT}
+    id::BoundaryIdentifier
+    data::BDT
+end
+
+struct DirichletCondition{BDT<:BoundaryDataType} <: BoundaryCondition{BDT}
+    id::BoundaryIdentifier
+    data::BDT
+end
+
+struct RobinCondition{BDT<:BoundaryDataType,T<:Real} <: BoundaryCondition{BDT}
+    id::BoundaryIdentifier
+    data::BDT
+    α::T
+    β::T
 end
