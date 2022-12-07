8ae94a8e37c392742c9866d3b802aba7be909c8f
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2022-12-07 21:56:00 +0100

    Make use of discretize in sat functions
    --HG--
    branch : feature/boundary_conditions

--- src/BoundaryConditions/sat.jl
+++ src/BoundaryConditions/sat.jl
@@ -6,57 +6,60 @@ Returns the `LazyTensor`s used to construct a SAT for the SBP operator `op` on
 """
 function sat_tensors end
 
+# TODO: Docs must be more specific in what this function does...
 """
-    sat(op, grid, bc::BoundaryCondition{ConstantBoundaryData{T}}, params...)
+    sat(op, grid, bc::BoundaryCondition, params...)
 
-Simultaneous-Approximation-Term for constant boundary data. f = sat(op, grid, bc) returns
-an anonymous function, such that f(u) is a `LazyTensorApplication` weakly imposing the BC.
+Simultaneous-Approximation-Term for general BoundaryCondition bc. f = sat(op, grid, bc) returns
+an anonymous function, such that f(t,u) is a `LazyTensorApplication` weakly imposing bc
+at time t.
 """
-function sat(op, grid, bc::BoundaryCondition{ConstantBoundaryData{T}}, params...) where T
+function sat(op, grid, bc::BoundaryCondition, params...)
     closure, penalty = sat_tensors(op, grid, bc, params...)
-    b_sz = size(boundary_grid(grid, bc.id))
-    g = fill(data(bc), b_sz)
-    if iszero(g)
-        return u -> closure(u)
-    else
-        return u -> closure(u) + penalty(g)
-    end
+    data_array = discretize(data(bc),boundary_grid(grid, bc.id))
+    return (t,u) -> closure(u) + penalty(data_array(t))
 end
 
-"""
-    sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...)
-
-Simultaneous-Approximation-Term for space-dependent boundary data. f = sat(op, grid, bc) returns
-an anonymous function, such that f(u) is a `LazyTensorApplication` weakly imposing the BC.
-"""
-function sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...) where T
-    closure, penalty = sat_tensors(op, grid, bc, params...)
-    g = data(bc)
-    return u -> closure(u) + penalty(g)
+function sat(op, grid, bc::BoundaryCondition{ZeroBoundaryData}, params...)
+    closure = sat_tensors(op, grid, bc, params...)
+    return (t,u) -> closure(u)
 end
 
-"""
-    sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...)
 
-Simultaneous-Approximation-Term for time-dependent boundary data. f = sat(op, grid, bc) returns
-an anonymous function, such that f(u,t) is a `LazyTensorApplication` weakly imposing the BC at time t.
-"""
-function sat(op, grid, bc::BoundaryCondition{TimeDependentBoundaryData{T}}, params...) where T
-    closure, penalty = sat_tensors(op, grid, bc, params...)
-    b_sz = size(boundary_grid(grid, bc.id))
-    b_vec = ones(eltype(grid), b_sz)
-    g = data(bc)
-    return (u,t) -> closure(u) + g(t)*penalty(b_vec)
-end
+# """
+#     sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...)
 
-"""
-    sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...)
+# Simultaneous-Approximation-Term for space-dependent boundary data. f = sat(op, grid, bc) returns
+# an anonymous function, such that f(u) is a `LazyTensorApplication` weakly imposing the BC.
+# """
+# function sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...) where T
+#     closure, penalty = sat_tensors(op, grid, bc, params...)
+#     g = data(bc)
+#     return u -> closure(u) + penalty(g)
+# end
 
-Simultaneous-Approximation-Term for space-time-dependent boundary data. f = sat(op, grid, bc) returns
-an anonymous function, such that f(u,t) is a `LazyTensorApplication` weakly imposing the BC at time t.
-"""
-function sat(op, grid, bc::BoundaryCondition{SpaceTimeDependentBoundaryData{T}}, params...) where T
-    closure, penalty = sat_tensors(op, grid, bc, params...)
-    g = data(bc)
-    return (u,t) -> closure(u) + penalty(g(t))
-end
+# """
+#     sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...)
+
+# Simultaneous-Approximation-Term for time-dependent boundary data. f = sat(op, grid, bc) returns
+# an anonymous function, such that f(u,t) is a `LazyTensorApplication` weakly imposing the BC at time t.
+# """
+# function sat(op, grid, bc::BoundaryCondition{TimeDependentBoundaryData{T}}, params...) where T
+#     closure, penalty = sat_tensors(op, grid, bc, params...)
+#     b_sz = size(boundary_grid(grid, bc.id))
+#     b_vec = ones(eltype(grid), b_sz)
+#     g = data(bc)
+#     return (u,t) -> closure(u) + g(t)*penalty(b_vec)
+# end
+
+# """
+#     sat(op, grid, bc::BoundaryCondition{SpaceDependentBoundaryData{T}}, params...)
+
+# Simultaneous-Approximation-Term for space-time-dependent boundary data. f = sat(op, grid, bc) returns
+# an anonymous function, such that f(u,t) is a `LazyTensorApplication` weakly imposing the BC at time t.
+# """
+# function sat(op, grid, bc::BoundaryCondition{SpaceTimeDependentBoundaryData{T}}, params...) where T
+#     closure, penalty = sat_tensors(op, grid, bc, params...)
+#     g = data(bc)
+#     return (u,t) -> closure(u) + penalty(g(t))
+# end
