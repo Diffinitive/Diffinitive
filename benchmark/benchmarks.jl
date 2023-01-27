0bc68a5fa8557aa70ca77ac767ba3124170c5681
Jonatan Werpers <jonatan@werpers.com>
2023-01-27 11:52:46 +0100

    Temporarily simplify the dummy benchmark suite
    --HG--
    branch : tooling/benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,20 +9,20 @@ SUITE["utf8"]["replace"] = @benchmarkable replace($teststr, "a" => "b")
 SUITE["utf8"]["join"] = @benchmarkable join($teststr, $teststr)
 SUITE["utf8"]["plots"] = BenchmarkGroup()
 
-SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
-SUITE["trigonometry"]["circular"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
+# SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
+# SUITE["trigonometry"]["circular"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
 
-SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
+# SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
 
 SUITE
 
