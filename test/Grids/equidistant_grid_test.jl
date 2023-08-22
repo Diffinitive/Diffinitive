330215cee2c5a322ac2cf5206be604ebfc661462
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2023-08-22 22:39:03 +0200

    Add more tests to indexing
    --HG--
    branch : bugfix/grids/complete_interface_impl

--- test/Grids/equidistant_grid_test.jl
+++ test/Grids/equidistant_grid_test.jl
@@ -22,6 +22,11 @@ using Sbplib.LazyTensors
 
         @test firstindex(g) == 1
         @test lastindex(g) == 101
+
+        @test g[:] == g[1:101]
+        
+        @test axes(g,1) == 1:101
+        @test axes(g) == (1:101,)
     end
 
     @testset "Iterator interface" begin
