bc07e901c7ed99bad183646b43cd458fe6f24ff3
Jonatan Werpers <jonatan@werpers.com>
2023-08-22 15:39:05 +0200

    REVIEW: Add a review comment
    --HG--
    branch : feature/grids/scalar_eval_on

--- src/Grids/grid.jl
+++ src/Grids/grid.jl
@@ -96,7 +96,7 @@ end
 
 Lazy evaluation of a scalar `f` on the grid.
 
-For concrete array grid functions `map(f,g)` can be used instead.
+For concrete array grid functions `map(f,g)` can be used instead. # REVIEW: The suggested code doesn't seem to work and I don't think it needs to. Probably enough to just delete the remark?
 """
 eval_on(g::Grid, f::Number) = return LazyTensors.LazyConstantArray(f, size(g))
 
