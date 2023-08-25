95a59659c49461421712274074eb27dd8214992d
Vidar Stiernström <vidar.stiernstrom@it.uu.se>
2023-08-25 16:08:21 +0200

    Test spatially variable data
    --HG--
    branch : feature/boundary_conditions

--- test/BoundaryConditions/boundary_condition_test.jl
+++ test/BoundaryConditions/boundary_condition_test.jl
@@ -10,9 +10,12 @@ using Sbplib.Grids
     (_,_,_,_,id_b,_) = boundary_identifiers(grid_3d)
 
     g = 3.14
-    f(x,y) = x^2+y^2
+    f(x,y,z) = x^2+y^2+z^2
     @test DirichletCondition(g,id_n) isa BoundaryCondition{Float64}
-    @test NeumannCondition(f,id_n) isa BoundaryCondition{<:Function}
+    @test NeumannCondition(f,id_b) isa BoundaryCondition{<:Function}
 
-   @test g*ones(11,1) ≈ discretize_data(grid_2d,DirichletCondition(g,id_n))
+    @test g*ones(11,1) ≈ discretize_data(grid_2d,DirichletCondition(g,id_n))
+    X = repeat(0:1/10:1, inner = (1,15))
+    Y = repeat(0:1/14:1, outer = (1,11))
+    @test map((x,y)->f(x,y,0), X,Y') ≈ discretize_data(grid_3d,NeumannCondition(f,id_b))
 end
