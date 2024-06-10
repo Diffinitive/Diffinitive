0c7c9e446c2ce55779cee85a2f973cadcb1c7298
Jonatan Werpers <jonatan@werpers.com>
2024-06-11 00:12:06 +0200

    REVIEW: Suggest changes to doc strings in sat.jl
    --HG--
    branch : feature/boundary_conditions

--- src/SbpOperators/boundary_conditions/sat.jl
+++ src/SbpOperators/boundary_conditions/sat.jl
@@ -1,11 +1,11 @@
 """
-    sat_tensors(op, grid, bc::BoundaryCondition, params...)
+    sat_tensors(op, grid, bc::BoundaryCondition; kwargs...)
 
-The tensor and boundary operator used to construct a simultaneous-approximation-term
-for imposing `bc` related to `op`.
+The penalty tensor and boundary operator used to construct a
+simultaneous-approximation-term for imposing `bc` related to `op`.
 
-For `penalty_tensor, L  = sat_tensors(...)` then `SAT = penalty_tensor*(L*u - g)`  where `g` 
-is the boundary data.
+For `penalty_tensor, L  = sat_tensors(...)` then `SAT(u,g) =
+penalty_tensor*(L*u - g)`  where `g` is the boundary data.
 """
 function sat_tensors end
 
@@ -13,10 +13,11 @@ function sat_tensors end
 """
     sat(op, grid, bc::BoundaryCondition; kwargs...)
 
-Simultaneous-Approximation-Term for a general `bc` to `op`. 
-Returns a function `SAT(u,g)` weakly imposing `bc` when added to `op*u`.
+Simultaneous-Approximation-Term for a general `bc` to `op`. Returns a function
+`SAT(u,g)` weakly imposing `bc` when added to `op*u`.
 
-`op` must implement the function `sat_tensors`.
+Internally `sat_tensors(op, grid, bc; ...)` is called to construct the
+necessary parts for the SAT.
 """
 function sat(op, grid, bc::BoundaryCondition; kwargs...)
     penalty_tensor, L = sat_tensors(op, grid, bc; kwargs...)
