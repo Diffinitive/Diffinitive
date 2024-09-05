076e4d45ef3ed60c22280e6760025ec6ab1a1436
Vidar Stiernström <vidar.stiernstrom@gmail.com>
2024-09-05 08:31:03 -0700

    Fix typos in docstring
    --HG--
    branch : feature/lazy_tensors/sparse_conversions

--- ext/SbplibSparseArrayKitExt.jl
+++ ext/SbplibSparseArrayKitExt.jl
@@ -10,9 +10,9 @@ using Tokens
     SparseArray(t::LazyTensor)
 
 The sparse tensor representation of `t` with range dimensions to the left and
-domain dimensions to the right. If `L` is a `LazyTensor`  with range and
+domain dimensions to the right. If `L` is a `LazyTensor` with range and
 domain dimension 2 and `v` a 2-tensor, then `A = SparseArray(t)` is
-constructed so that `∑ₖ∑ₗA[i,j,k,l]*v[k,l] == L*v`§
+constructed so that `∑ₖ∑ₗA[i,j,k,l]*v[k,l] == L*v`.
 """
 function SparseArrayKit.SparseArray(t::LazyTensor)
     v = ArrayToken(:v, domain_size(t)...)
