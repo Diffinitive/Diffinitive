f3b13204779afc08f5fb2ab798d36611955de12b
Jonatan Werpers <jonatan@werpers.com>
2024-10-14 23:26:49 +0200

    Resolve ambiguities for addition and subtraction of LazyArrays
    --HG--
    branch : feature/jet_aqua

--- src/Diffinitive.jl
+++ src/Diffinitive.jl
@@ -10,4 +10,14 @@ export LazyTensors
 export Grids
 export SbpOperators
 
+
+
+# Aqua.jl fixes
+using StaticArrays
+using .LazyTensors
+Base.:+(a::StaticArray, b::LazyArray) = a +̃ b
+Base.:+(a::LazyArray, b::StaticArray) = a +̃ b
+Base.:-(a::StaticArray, b::LazyArray) = a -̃ b
+Base.:-(a::LazyArray, b::StaticArray) = a -̃ b
+
 end
