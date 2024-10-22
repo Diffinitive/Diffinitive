c2214f16ae526e9ad880aa06fa3d2573c74667e0
Jonatan Werpers <jonatan@werpers.com>
2024-10-22 15:43:07 +0200

    Add test stubs
    --HG--
    branch : feature/grids/manifolds

--- test/Grids/manifolds_test.jl
+++ test/Grids/manifolds_test.jl
@@ -72,6 +72,26 @@ end
     @test ndims(c) == 2
 end
 
-@testset "Atlas" begin
+@testset "CartesianAtlas" begin
+    @test_broken false
+end
 
+@testset "UnstructuredAtlas" begin
+    @test_broken false
+end
+
+@testset "Line" begin
+    @test_broken false
+end
+
+@testset "LineSegment" begin
+    @test_broken false
+end
+
+@testset "Circle" begin
+    @test_broken false
+end
+
+@testset "TransfiniteInterpolationSurface" begin
+    @test_broken false
 end
