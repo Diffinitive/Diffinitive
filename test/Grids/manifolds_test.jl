ba98355017e35bdc68aea7fb0a22340e96872808
Jonatan Werpers <jonatan@werpers.com>
2025-02-03 15:39:26 +0100

    Delete functions related to geometries as they don't belong on this branch
    --HG--
    branch : feature/grids/manifolds

--- test/Grids/manifolds_test.jl
+++ test/Grids/manifolds_test.jl
@@ -43,27 +43,3 @@ end
 @testset "UnstructuredAtlas" begin
     @test_broken false
 end
-
-@testset "Line" begin
-    @test_broken false
-end
-
-@testset "LineSegment" begin
-    @test_broken false
-end
-
-@testset "linesegments" begin
-    @test_broken false
-end
-
-@testset "polygon_edges" begin
-    @test_broken false
-end
-
-@testset "Circle" begin
-    @test_broken false
-end
-
-@testset "TransfiniteInterpolationSurface" begin
-    @test_broken false
-end
