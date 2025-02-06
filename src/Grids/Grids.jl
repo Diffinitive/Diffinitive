f8e271bbda809cb4efe8f8c189fed88d2f9c4334
Jonatan Werpers <jonatan@werpers.com>
2025-02-06 15:48:03 +0100

    Fix order of includes
    --HG--
    branch : feature/grids/manifolds

--- src/Grids/Grids.jl
+++ src/Grids/Grids.jl
@@ -75,12 +75,12 @@ export mapped_grid
 export metric_tensor
 
 include("parameter_space.jl")
-include("manifolds.jl")
 include("grid.jl")
+include("multiblockgrids.jl")
+include("manifolds.jl")
 include("tensor_grid.jl")
 include("equidistant_grid.jl")
 include("zero_dim_grid.jl")
 include("mapped_grid.jl")
-include("multiblockgrids.jl")
 
 end # module
