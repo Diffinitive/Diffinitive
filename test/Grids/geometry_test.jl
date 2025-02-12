f68f274ea1fb7c2348e17a2d1680c4f06b810536
Jonatan Werpers <jonatan@werpers.com>
2025-02-12 15:30:47 +0100

    Add constructor testset
    --HG--
    branch : feature/grids/geometry_functions

--- test/Grids/geometry_test.jl
+++ test/Grids/geometry_test.jl
@@ -1,23 +1,40 @@
 @testset "Line" begin
     @test_broken false
+    @testset "Constructors" begin
+    end
 end
 
 @testset "LineSegment" begin
+    @testset "Constructors" begin
+    end
+
     @test_broken false
 end
 
 @testset "linesegments" begin
+    @testset "Constructors" begin
+    end
+
     @test_broken false
 end
 
 @testset "polygon_edges" begin
+    @testset "Constructors" begin
+    end
+
     @test_broken false
 end
 
 @testset "Circle" begin
+    @testset "Constructors" begin
+    end
+
     @test_broken false
 end
 
 @testset "TransfiniteInterpolationSurface" begin
+    @testset "Constructors" begin
+    end
+
     @test_broken false
 end
