80bc4878bea0ab3ce67bfd83c560de5939a4ca2d
Jonatan Werpers <jonatan@werpers.com>
2025-02-26 23:12:17 +0100

    Make the output of update_manifest.jl nicer

--- update_manifest.jl
+++ update_manifest.jl
@@ -1,12 +1,22 @@
 using Pkg
 
-function update_directory(d)
-    Pkg.activate(d)
-    Pkg.update()
+function update_and_log(root, d)
+    printstyled("Updating '$d'"; color=:cyan, bold=true)
+    println()
+    dp = joinpath(root, d)
+
+    update_directory(dp)
     println()
 end
 
-update_directory(".")
-update_directory("benchmark")
-update_directory("docs")
-update_directory("test")
+function update_directory(d)
+    Pkg.activate(d)
+    Pkg.update()
+end
+
+root = @__DIR__
+update_and_log(root, ".")
+update_and_log(root, "benchmark")
+update_and_log(root, "docs")
+update_and_log(root, "test")
+
