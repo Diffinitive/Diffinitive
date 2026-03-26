1f6cda241947efd34e2d4d4799297d7278701ded
Jonatan Werpers <jonatan@werpers.com>
2026-03-26 10:57:47 +0100

    Fix bugs in elastic_isotropic
    --HG--
    branch : feature/sbp_operators/vector_operators

--- src/SbpOperators/volumeops/derivatives/elastic_isotropic.jl
+++ src/SbpOperators/volumeops/derivatives/elastic_isotropic.jl
@@ -6,30 +6,28 @@ function elastic_isotropic(g::TensorGrid, λ, μ, stencil_set)
 
     N = ndims(g)
 
-    ∂ₖμ∂ₖ = sum(1:N) do k
+    Σₖ∂ₖμ∂ₖ = sum(1:N) do k
         second_derivative_variable(g,μ,stencil_set,k)
     end
 
     Λ = DiagonalTensor(λ)
     M = DiagonalTensor(μ)
 
-
     Ds = map(Iterators.product(1:N, 1:N)) do (i,j)
         ∂ᵢ = first_derivative(g, stencil_set, i)
         ∂ⱼ = first_derivative(g, stencil_set, j)
         if i == j
             ∂ⱼμ∂ᵢ = second_derivative_variable(g,μ,stencil_set,i)
-            # ∂ₖμ∂ₖδᵢⱼ = ∂ₖμ∂ₖ
-            return ∂ᵢ∘Λ∘∂ⱼ + ∂ⱼμ∂ᵢ + ∂ₖμ∂ₖ
+            # Σₖ∂ₖμ∂ₖδᵢⱼ = Σₖ∂ₖμ∂ₖ
+            return ∂ᵢ∘Λ∘∂ⱼ + ∂ⱼμ∂ᵢ + Σₖ∂ₖμ∂ₖ
         else
-            ∂ⱼμ∂ᵢ = ∂ⱼ∘M∘∂ᵢ
-            # ∂ₖμ∂ₖδᵢⱼ = ZeroTensor()
-            return ∂ᵢ∘Λ∘∂ⱼ + ∂ⱼμ∂ᵢ
+            # ∂ⱼμ∂ᵢ = ∂ⱼ∘M∘∂ᵢ
+            # Σₖ∂ₖμ∂ₖδᵢⱼ = ZeroTensor()
+            return ∂ᵢ∘Λ∘∂ⱼ + ∂ⱼ∘M∘∂ᵢ
         end
 
-        return ∂ᵢ∘Λ∘∂ⱼ + ∂ⱼμ∂ᵢ + ∂ₖμ∂ₖδᵢⱼ
+        # return ∂ᵢ∘Λ∘∂ⱼ + ∂ⱼμ∂ᵢ + Σₖ∂ₖμ∂ₖδᵢⱼ
     end
 
     return MatrixTensor(Ds)
 end
-
