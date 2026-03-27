4705c285c98fc34988f43eb9ed85c205ff72451f
Jonatan Werpers <jonatan@werpers.com>
2026-03-27 15:23:08 +0100

    Start adding elastic_isotropic for mapped grid
    --HG--
    branch : feature/sbp_operators/vector_operators

--- src/SbpOperators/volumeops/derivatives/elastic_isotropic.jl
+++ src/SbpOperators/volumeops/derivatives/elastic_isotropic.jl
@@ -1,3 +1,5 @@
+# Tensor grid
+# ===========
 function elastic_isotropic(g::TensorGrid, λ, μ, stencil_set)
     # ∂ᵢλ∂ⱼuⱼ + ∂ⱼμ∂ᵢuⱼ + ∂ₖμ∂ₖuᵢ
     # =>
@@ -63,6 +65,65 @@ function traction_isotropic(g::TensorGrid, λ, μ, stencil_set, boundary)
 end
 
 
+# Mapped grid
+# ===========
+function elastic_isotropic(g::MappedGrid, λ, μ, stencil_set)
+    # Lᵢⱼuⱼ = (∂ᵢλ∂ⱼ + ∂ⱼμ∂ᵢ + ∂ₖμ∂ₖδᵢⱼ) uⱼ
+    #
+    #       = J⁻¹(∂̃ₖλJg̃ᵏⁿⁱʲ∂̃ₙ + ∂̃ₖμJg̃ᵏⁿʲⁱ∂̃ₙ + δᵢⱼ∂̃ₖμJg̃ᵏⁿˢˢ∂̃ₙ) uⱼ
+    #
+    #       = J⁻¹(∂̃ₖλJg̃ᵏⁿⁱʲ∂̃ₙ + ∂̃ₖμJg̃ᵏⁿʲⁱ∂̃ₙ + δᵢⱼ∂̃ₖμJgᵏⁿ∂̃ₙ) uⱼ
+    # where g̃ᵏⁿⁱʲ = ∂ξₖ/∂xᵢ ∂ξₙ/∂xⱼ
+    # and gᵏⁿ = ∂ξₖ/∂xₛ ∂ξₙ/∂xₛ
+    N = ndims(g)
+
+    ∂ξ∂x = map(inv, jacobian(g))
+    J = map(det, jacobian(g))
+    J⁻¹ = map(inv, J)
+
+    λJg̃ = map(CartesianIndices((N,N,N,N))) do I
+        k,n,i,j = Tuple(I)
+
+        ∂ξₖ∂xᵢ = componentview(∂ξ∂x,k,i)
+        ∂ξₙ∂xⱼ = componentview(∂ξ∂x,n,j)
+
+        λ.*J.*∂ξₖ∂xᵢ.*∂ξₙ∂xⱼ
+    end
+
+    g = map(CartesianIndices((N,N))) do I
+        a,b = Tuple(I)
+    end
+
+
+    g(a,b,c,d) = nothing
+
+
+    ∂̃(i) = first_derivative(logical_grid(g), stencil_set, i)
+    ∂̃²(σ,i) = second_derivative_variable(logical_grid(g), σ, stencil_set, i)
+    ∂̃σ∂̃(i,σ,j) = i==j ? ∂̃²(σ,i) : ∂̃(i)∘DiagonalTensor(σ)∘∂̃(j)
+
+    δ(i,j) = i==j ? IdentityTensor(size(lg)) : ZeroTensor(size(lg))
+
+
+    return MatrixTensor(N, N) do i,j
+        sum(1:N) do k
+            sum(1:N) do n
+                ∂̃ₖλJgᵏⁿⁱʲ∂̃ₙ = ∂̃(k)∘DiagonalTensor(λJg̃[k,n,i,j])∘∂̃(n)
+
+                ∂̃ₖμJgᵏⁿʲⁱ∂̃ₙ = ∂̃σ∂̃(k, μJg̃[k,n,j,i], n)
+
+                δᵢⱼ∂̃ₖμJgᵏⁿˢˢ∂̃ₙ = δ(i,j)∘∂̃σ∂̃(k,μJg[k,n],n) # TODO: Kolla att δ försvinner helt
+
+                return ∂̃ₖλJgᵏⁿⁱʲ∂̃ₙ + ∂̃ₖμJgᵏⁿʲⁱ∂̃ₙ + δᵢⱼ∂̃ₖμJgᵏⁿˢˢ∂̃ₙ
+            end
+        end
+    end
+end
+
+
+
+# Helpers
+# =======
 function boundary_gradient(g, stencil_set, boundary)
     e = boundary_restriction(g, stencil_set, boundary)
     return map(1:ndims(g)) do i
@@ -76,3 +137,6 @@ function boundary_gradient(g, stencil_set, boundary)
         end
     end
 end
+
+
+
