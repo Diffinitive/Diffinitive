88ba738b339da969d096037b81c51c4db43840a0
Jonatan Werpers <jonatan@werpers.com>
2026-07-24 00:10:42 +0200

    Introduce a new type BoundaryOperatorAdjoint to allow removing apply_transpose for BoundaryOperator
    --HG--
    branch : refactor/lazy_tensors/adjoint

--- src/SbpOperators/boundaryops/boundary_operator.jl
+++ src/SbpOperators/boundaryops/boundary_operator.jl
@@ -21,8 +21,8 @@ Constructs the BoundaryOperator with stencil `closure_stencil` for a
 `boundary`.
 """
 function BoundaryOperator(grid::EquidistantGrid, closure_stencil::Stencil, boundary::BoundaryIdentifier)
-    ST = typeof(closure_stencil)
     B = typeof(boundary)
+    ST = typeof(closure_stencil)
     return BoundaryOperator{B,ST}(closure_stencil,size(grid)[1])
 end
 
@@ -36,6 +36,10 @@ closure_size(op::BoundaryOperator) = length(op.stencil)
 LazyTensors.range_size(op::BoundaryOperator) = ()
 LazyTensors.domain_size(op::BoundaryOperator) = (op.size,)
 
+function Base.adjoint(op::BoundaryOperator{B,ST}) where {B,ST}
+    return BoundaryOperatorAdjoint{B,ST}(op.stencil, op.size)
+end
+
 function LazyTensors.apply(op::BoundaryOperator{LowerBoundary}, v::AbstractVector)
     apply_stencil(op.stencil,v,1)
 end
@@ -44,20 +48,39 @@ function LazyTensors.apply(op::BoundaryOperator{UpperBoundary}, v::AbstractVecto
     apply_stencil_backwards(op.stencil,v,op.size)
 end
 
-function LazyTensors.apply_transpose(op::BoundaryOperator{LowerBoundary}, v::AbstractArray{<:Any,0}, i::Index{Lower})
+"""
+    BoundaryOperatorAdjoint{B} <: LazyTensor{1,0}
+
+Implements the adjoint of a `BoundaryOperator`.
+"""
+struct BoundaryOperatorAdjoint{B<:BoundaryIdentifier,ST<:Stencil} <: LazyTensor{1,0}
+    stencil::ST
+    size::Int
+end
+
+closure_size(op::BoundaryOperatorAdjoint) = length(op.stencil)
+
+LazyTensors.range_size(op::BoundaryOperatorAdjoint) = (op.size,)
+LazyTensors.domain_size(op::BoundaryOperatorAdjoint) = ()
+
+function Base.adjoint(op::BoundaryOperatorAdjoint{B,ST}) where {B,ST}
+    return BoundaryOperator{B,ST}(op.stencil, op.size)
+end
+
+function LazyTensors.apply(op::BoundaryOperatorAdjoint{LowerBoundary}, v::AbstractArray{<:Any,0}, i::Index{Lower})
     return op.stencil[Int(i)-1]*v[]
 end
 
-function LazyTensors.apply_transpose(op::BoundaryOperator{UpperBoundary}, v::AbstractArray{<:Any,0}, i::Index{Upper})
+function LazyTensors.apply(op::BoundaryOperatorAdjoint{UpperBoundary}, v::AbstractArray{<:Any,0}, i::Index{Upper})
     return op.stencil[op.size - Int(i)]*v[]
 end
 
 # Catch all combinations of Lower, Upper and Interior not caught by the two previous methods.
-function LazyTensors.apply_transpose(op::BoundaryOperator, v::AbstractArray{<:Any,0}, i::Index)
+function LazyTensors.apply(op::BoundaryOperatorAdjoint, v::AbstractArray{<:Any,0}, i::Index)
     return zero(eltype(v))
 end
 
-function LazyTensors.apply_transpose(op::BoundaryOperator, v::AbstractArray{<:Any,0}, i)
+function LazyTensors.apply(op::BoundaryOperatorAdjoint, v::AbstractArray{<:Any,0}, i)
     r = getregion(i, closure_size(op), op.size)
-    apply_transpose(op, v, Index(i,r))
+    apply(op, v, Index(i,r))
 end
