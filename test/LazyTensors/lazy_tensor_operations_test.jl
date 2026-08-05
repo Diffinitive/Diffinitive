43f2b971774402b4f4a0c6e1b5b2e8dcee828ee6
Vidar Stiernström <vidar.stiernstrom@gmail.com>
2026-08-05 09:07:23 +0200

    Update TensorSum tests
    --HG--
    branch : refactor/lazy_tensors/operator_simplifications

--- test/LazyTensors/lazy_tensor_operations_test.jl
+++ test/LazyTensors/lazy_tensor_operations_test.jl
@@ -198,22 +198,24 @@ end
         C = ScalingTensor(3.0, (3,))
         D = ScalingTensor(4.0, (3,))
 
-        @test A+B+C+D isa TensorSum
+        @test A+B+C+D == TensorSum(A,B,C,D)
+        @test TensorSum(A,B,C,D) != TensorSum(TensorSum(A,B),TensorSum(C,D)) # Constructor does not flatten
         @test length((A+B+C+D).tms) == 4
 
 
-        @test A+B-C+D isa TensorSum
+        @test A+B-C+D == TensorSum(A,B,-C,D)
         @test length((A+B-C+D).tms) == 4
 
         v = rand(3)
         @test (A+B-C+D)*v == 1v + 2v - 3v + 4v
 
 
-        @test -A-B-C-D isa TensorSum
+        @test -A-B-C-D == TensorSum(-A,-B,-C,-D)
         @test length((-A-B-C-D).tms) == 4
 
         v = rand(3)
         @test (-A-B-C-D)*v == -1v - 2v - 3v - 4v
+
     end
 
     @testset "ZeroTensor arguments" begin
