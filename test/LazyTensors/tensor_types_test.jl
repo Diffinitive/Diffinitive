1fd2a6f0e85d091f0351faaebe927a5682ad1326
Jonatan Werpers <jonatan@werpers.com>
2026-08-12 10:03:15 +0200

    Move TupleTable to combined tuple utils file
    --HG--
    branch : feature/lazy_tensors/matrix_of_operators

--- test/LazyTensors/tensor_types_test.jl
+++ test/LazyTensors/tensor_types_test.jl
@@ -1,6 +1,6 @@
 using Test
 using Diffinitive.LazyTensors
-using Diffinitive.LazyTensors: TupleTable, tuple_range
+using Diffinitive.LazyTensors: TupleTable
 using BenchmarkTools
 using StaticArrays
 
@@ -255,88 +255,6 @@ end
     # @inferred (B̃*v)[2]
 end
 
-@testset "TupleTable" begin
-    @testset "Constructors" begin
-        @test TupleTable((1,2,3)) isa TupleTable{1,3}
-        @test TupleTable((1,2),(3,4)) isa TupleTable{2,2}
-        @test TupleTable((1,2,3),(3,4,5)) isa TupleTable{2,3}
-        @test TupleTable((1,2),(3,4),(5,6)) isa TupleTable{3,2}
-
-        @test TupleTable([1 2; 3 4]) isa TupleTable{2,2}
-        @test TupleTable([1 2 3; 3 4 5]) isa TupleTable{2,3}
-
-        @test_throws DimensionMismatch("All rows must have the same length") TupleTable((1,2),(1,2,3))
-    end
-
-    @testset "size" begin
-        @test size(TupleTable((1,2,3))) == (1,3)
-        @test size(TupleTable((1,2),(3,4))) == (2,2)
-        @test size(TupleTable((1,2,3),(3,4,5))) == (2,3)
-        @test size(TupleTable((1,2),(3,4),(5,6))) == (3,2)
-
-        @test size(TupleTable{1,3}) == (1,3)
-        @test size(TupleTable{2,2}) == (2,2)
-        @test size(TupleTable{2,3}) == (2,3)
-        @test size(TupleTable{3,2}) == (3,2)
-    end
-
-    @testset "getindex" begin
-        tt = TupleTable((1,2,3))
-        @test tt[1,1] == 1
-        @test tt[1,2] == 2
-        @test tt[1,3] == 3
-        @test_throws BoundsError tt[2,2]
-
-        @test tt[1,:] == (1,2,3)
-        @test_throws BoundsError tt[2,:]
-
-        tt = TupleTable((1,2),(3,4))
-        @test tt[1,1] == 1
-        @test tt[1,2] == 2
-        @test tt[2,1] == 3
-        @test tt[2,2] == 4
-        @test_throws BoundsError tt[3,2]
-        @test_throws BoundsError tt[2,3]
-
-        @test tt[1,:] == (1,2)
-        @test tt[2,:] == (3,4)
-        @test_throws BoundsError tt[3,:]
-
-        tt = TupleTable((1,2,3),(3,4,5))
-        @test tt[1,3] == 3
-        @test tt[2,1] == 3
-        @test tt[2,3] == 5
-
-        tt = TupleTable((1,2),(3,4),(5,6))
-        @test tt[1,1] == 1
-        @test tt[2,2] == 4
-        @test tt[3,1] == 5
-    end
-
-    @testset "Base.adjoint" begin
-        tt = TupleTable((1+1im,2+2im,3+3im),(4+4im,5+5im,6+6im))
-        expected = TupleTable((1-1im, 4-4im),(2-2im, 5-5im),(3-3im, 6-6im))
-        @test adjoint(tt) == expected
-    end
-
-    @testset "Base.:(==)" begin
-        @test TupleTable((1,2),(3,4)) == TupleTable((1,2),(3,4))
-        @test TupleTable(([1,2],2),(3,4)) == TupleTable(([1,2],2),(3,4))
-
-        @test TupleTable((2,2),(3,4)) != TupleTable((1,2),(3,4))
-        @test TupleTable(([2,2],2),(3,4)) != TupleTable(([1,2],2),(3,4))
-    end
-
-    @testset "Base.:+" begin
-        A = TupleTable((1,2),(3,4))
-        @test A+A == TupleTable((2,4),(6,8))
-
-        A = TupleTable((1,2),(3,4))
-        B = TupleTable((3,2),(1,0))
-        @test A+B == TupleTable((4,4),(4,4))
-    end
-end
-
 @testset "VectorTensor" begin
     @testset "Constructors" begin
         s = [4., 6., 5., 6., 7.]
@@ -530,15 +448,4 @@ end
         )
         @test A != B
     end
-
-end
-
-@testset "tuple_range()" begin
-    @test tuple_range(1) == (1,)
-    @test tuple_range(2) == (1,2)
-    @test tuple_range(5) == (1,2,3,4,5)
-
-    @test tuple_range(Val(1)) == (1,)
-    @test tuple_range(Val(2)) == (1,2)
-    @test tuple_range(Val(5)) == (1,2,3,4,5)
 end
--- test/LazyTensors/tuple_utils_test.jl
+++ test/LazyTensors/tuple_utils_test.jl
@@ -1,5 +1,6 @@
 using Test
 using Diffinitive.LazyTensors
+using Diffinitive.LazyTensors: TupleTable, tuple_range
 
 @testset "split_index" begin
     @test LazyTensors.split_index(2,1,2,2, 1,2,3,4,5,6) == ((1,2,:,5,6),(3,4))
@@ -80,4 +81,96 @@ end
     @test LazyTensors.right_pad_tuple((3,2), 1, 6) == (3,2,1,1,1,1)
     err_msg = "Can't pad tuple of length 2 to 0 elements"
     @test_throws DomainError(0,err_msg) LazyTensors.right_pad_tuple((1,2), 0, 0) == (1,2)
+end
+
+@testset "TupleTable" begin
+    @testset "Constructors" begin
+        @test TupleTable((1,2,3)) isa TupleTable{1,3}
+        @test TupleTable((1,2),(3,4)) isa TupleTable{2,2}
+        @test TupleTable((1,2,3),(3,4,5)) isa TupleTable{2,3}
+        @test TupleTable((1,2),(3,4),(5,6)) isa TupleTable{3,2}
+
+        @test TupleTable([1 2; 3 4]) isa TupleTable{2,2}
+        @test TupleTable([1 2 3; 3 4 5]) isa TupleTable{2,3}
+
+        @test_throws DimensionMismatch("All rows must have the same length") TupleTable((1,2),(1,2,3))
+    end
+
+    @testset "size" begin
+        @test size(TupleTable((1,2,3))) == (1,3)
+        @test size(TupleTable((1,2),(3,4))) == (2,2)
+        @test size(TupleTable((1,2,3),(3,4,5))) == (2,3)
+        @test size(TupleTable((1,2),(3,4),(5,6))) == (3,2)
+
+        @test size(TupleTable{1,3}) == (1,3)
+        @test size(TupleTable{2,2}) == (2,2)
+        @test size(TupleTable{2,3}) == (2,3)
+        @test size(TupleTable{3,2}) == (3,2)
+    end
+
+    @testset "getindex" begin
+        tt = TupleTable((1,2,3))
+        @test tt[1,1] == 1
+        @test tt[1,2] == 2
+        @test tt[1,3] == 3
+        @test_throws BoundsError tt[2,2]
+
+        @test tt[1,:] == (1,2,3)
+        @test_throws BoundsError tt[2,:]
+
+        tt = TupleTable((1,2),(3,4))
+        @test tt[1,1] == 1
+        @test tt[1,2] == 2
+        @test tt[2,1] == 3
+        @test tt[2,2] == 4
+        @test_throws BoundsError tt[3,2]
+        @test_throws BoundsError tt[2,3]
+
+        @test tt[1,:] == (1,2)
+        @test tt[2,:] == (3,4)
+        @test_throws BoundsError tt[3,:]
+
+        tt = TupleTable((1,2,3),(3,4,5))
+        @test tt[1,3] == 3
+        @test tt[2,1] == 3
+        @test tt[2,3] == 5
+
+        tt = TupleTable((1,2),(3,4),(5,6))
+        @test tt[1,1] == 1
+        @test tt[2,2] == 4
+        @test tt[3,1] == 5
+    end
+
+    @testset "Base.adjoint" begin
+        tt = TupleTable((1+1im,2+2im,3+3im),(4+4im,5+5im,6+6im))
+        expected = TupleTable((1-1im, 4-4im),(2-2im, 5-5im),(3-3im, 6-6im))
+        @test adjoint(tt) == expected
+    end
+
+    @testset "Base.:(==)" begin
+        @test TupleTable((1,2),(3,4)) == TupleTable((1,2),(3,4))
+        @test TupleTable(([1,2],2),(3,4)) == TupleTable(([1,2],2),(3,4))
+
+        @test TupleTable((2,2),(3,4)) != TupleTable((1,2),(3,4))
+        @test TupleTable(([2,2],2),(3,4)) != TupleTable(([1,2],2),(3,4))
+    end
+
+    @testset "Base.:+" begin
+        A = TupleTable((1,2),(3,4))
+        @test A+A == TupleTable((2,4),(6,8))
+
+        A = TupleTable((1,2),(3,4))
+        B = TupleTable((3,2),(1,0))
+        @test A+B == TupleTable((4,4),(4,4))
+    end
+end
+
+@testset "tuple_range()" begin
+    @test tuple_range(1) == (1,)
+    @test tuple_range(2) == (1,2)
+    @test tuple_range(5) == (1,2,3,4,5)
+
+    @test tuple_range(Val(1)) == (1,)
+    @test tuple_range(Val(2)) == (1,2)
+    @test tuple_range(Val(5)) == (1,2,3,4,5)
 end
